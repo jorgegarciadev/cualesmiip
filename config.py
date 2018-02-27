0785213a6538124bb9a543438531f10ce20d341f
Jorge García <db0035@icloud.com>
2018-02-27 06:51:38 +0100

    CSFR generator

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
-import os
+import os, random, string
 
 def loadVariable(variable1, variable2):
     try:
         return os.environ[variable1]
     except:
         return variable2
-        
+
+def generateCSFRKEY(lenght):
+  key = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(lenght)])
+  return key
 
 CSRF_ENABLED = True
-SECRET_KEY = loadVariable("CSFR_KEY", "tonto-el-que-lo-lea")
+SECRET_KEY = loadVariable("CSFR_KEY", generateCSFRKEY(32))
